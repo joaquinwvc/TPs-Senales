e88bd6f28bb787033853f78be5d81e1d79487f1d
jose <jovall@fi.uba.ar>
2026-09-19 20:16:49 -0300

    Modificación menor de generate tone

--- tp1/ej4/generador_de_tonos_ej4.py
+++ tp1/ej4/generador_de_tonos_ej4.py
@@ -1,11 +1,12 @@
 import numpy as np
 from scipy.io import wavfile
 
-def generate_tone(frequency, duration, sample_rate=44100):
+def generate_tone(amplitude, frequency, duration, sample_rate=44100):
     """
     Genera un tono en forma de onda coseno.
 
     Argumentos:
+        amplitude (float): Multiplica por un factor de escala la amplitud de la señal
         frequency (float): Frecuencia del tono en Hz.
         duration (float): Duración del tono en segundos.
         sample_rate (int): Frecuencia de muestreo en Hz (por defecto 44100).
@@ -16,7 +17,7 @@ def generate_tone(frequency, duration, sample_rate=44100):
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     
     # Señal x(t) = cos(2 * pi * f * t)
-    signal = np.cos(2 * np.pi * frequency * t)
+    signal = amplitude * np.cos(2 * np.pi * frequency * t)
     
     # Normalizar la señal para guardarla como audio de 16 bits
     signal_normalized = np.int16(signal * 32767)
@@ -34,24 +35,29 @@ def save_audio(filename, signal, sample_rate=44100):
     wavfile.write(filename, sample_rate, signal)
     print(f"Archivo generado exitosamente: {filename}")
 
-def main():
-    # Parámetros comunes
-    duracion = 3.0  # Al menos 3 segundos como pide el enunciado
-    fs = 44100      # Frecuencia de muestreo (CD quality)
-
-    # --- Generación de x1(t) = cos(2*pi*440*t) ---
-    frecuencia1 = 440
-    print(f"Generando tono de {frecuencia1} Hz...")
-    senial1 = generate_tone(frecuencia1, duracion, fs)
-    save_audio("x1_tono_440Hz.wav", senial1, fs)
-
-    # --- Generación de x2(t) = cos(2*pi*880*t) ---
-    frecuencia2 = 880
-    print(f"Generando tono de {frecuencia2} Hz...")
-    senial2 = generate_tone(frecuencia2, duracion, fs)
-    save_audio("x2_tono_880Hz.wav", senial2, fs)
+def ejc4():
+     # Parámetros comunes
+        duracion = 3.0  # Al menos 3 segundos como pide el enunciado
+        fs = 44100      # Frecuencia de muestreo (CD quality)
     
-    print("\n¡Proceso finalizado! Los audios están listos para ser escuchados.")
+        # --- Generación de x1(t) = cos(2*pi*440*t) ---
+        frecuencia1 = 440
+        amplitud1 = 1.0
+        print(f"Generando tono de {frecuencia1} Hz...")
+        senial1 = generate_tone(amplitud1, frecuencia1, duracion, fs)
+        save_audio("x1_tono_440Hz.wav", senial1, fs)
+    
+        # --- Generación de x2(t) = cos(2*pi*880*t) ---
+        frecuencia2 = 880
+        amplitud2 = 1.0
+        print(f"Generando tono de {frecuencia2} Hz...")
+        senial2 = generate_tone(amplitud2, frecuencia2, duracion, fs)
+        save_audio("x2_tono_880Hz.wav", senial2, fs)
+        
+        print("\n¡Proceso finalizado! Los audios están listos para ser escuchados.")
+
+def main():
+   ejc4()
 
 if __name__ == "__main__":
     main()
